3a72d6d12a2f9ce79a7977e9481e277be8c7a12c
Yuku Kotani <yukukotani@gmail.com>
2019-12-19 20:44:13 +0900

    Implement simple health check

--- src/main/kotlin/com/sohosai/sos/infrastructure/Routes.kt
+++ src/main/kotlin/com/sohosai/sos/infrastructure/Routes.kt
@@ -3,6 +3,8 @@ package com.sohosai.sos.infrastructure
 import com.sohosai.sos.presenter.controller.GraphQLController
 import io.ktor.application.call
 import io.ktor.auth.authenticate
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.routing.Routing
 import io.ktor.routing.get
 import io.ktor.routing.post
@@ -13,5 +15,6 @@ internal fun Routing.routes() {
     authenticate(optional = true) {
         get("/graphql") { graphQLController.get(call) }
         post("/graphql") { graphQLController.post(call) }
+        get("/health-check") { call.respond(HttpStatusCode.OK) }
     }
 }
